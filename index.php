6f1ff7015915e95a9beeabce945d878031011c15
Omar <omar@ingenieros.com>
2025-08-24 22:02:39 -0600

    remover gob

--- index.php
+++ index.php
@@ -6,19 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ejemplo de nueva página para GOB.mx</title>
-
-
     <!-- CSS -->
     <link href="/favicon.ico" rel="shortcut icon">
-    <link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
-
-    <!-- Respond.js soporte de media queries para Internet Explorer 8 -->
-    <!-- ie8.js EventTarget para cada nodo en Internet Explorer 8 -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/ie8/0.2.2/ie8.js"></script>
-    <![endif]-->
-
 </head>
 
 <body>
@@ -103,9 +92,15 @@
         echo "Hello World!";
         ?>
     </main>
+    <div class="site-copyright text-center">
+	<p>Algunos derechos reservados © 2025</p>
+	<p>
+		Km 1 Carretera Gutiérrez Zamora-Tecolutla, Gutiérrez Zamora,Ver. Mexico C.P. 93557
+	</p>
 
+    </div>
     <!-- JS -->
-    <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+    <script src="></script>
 
 </body>
 
