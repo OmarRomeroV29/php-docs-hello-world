1e6900c65b26f0be65678fb5f7da782362eb281b
Omar <omar@ingenieros.com>
2025-08-24 18:36:44 -0600

    con menu

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,20 +19,49 @@
       <script src="https://cdnjs.cloudflare.com/ajax/libs/ie8/0.2.2/ie8.js"></script>
     <![endif]-->
 
-  </head>
-  <body>
+</head>
 
+<body>
+    <nav class="navbar navbar-inverse sub-navbar navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#subenlaces">
+                    <span class="sr-only">Interruptor de Navegación</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">Semarnat</a>
+            </div>
+            <div class="collapse navbar-collapse" id="subenlaces">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Enlace</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Desplegable <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Acción</a></li>
+                            <li><a href="#">Otra acción</a></li>
+                            <li><a href="#">Algo más aquí</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Enlace separado</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <!-- Contenido -->
     <main class="page">
-      <div class="container">...</div>
-      <?php
+        <div class="container">...</div>
+        <?php
 
         echo "Hello World!";
-         ?>
+        ?>
     </main>
 
     <!-- JS -->
     <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
 
-  </body>
+</body>
+
 </html>
